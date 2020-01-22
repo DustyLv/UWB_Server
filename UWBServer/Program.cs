02075dce57d94a4ee99a152b350bd9221607e8d3
Lauris Taube <lauristaube@gmail.com>
2020-01-22 18:48:04 +0200

    Created VS Console server

---
+++ UWBServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWBServer
+{
+    class Program
+    {
+        static void Main(string[] args)     
+        {
+            Console.Title = "UWB Server";
+
+            Server.Start(50, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
